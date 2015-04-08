2a599edcf154359abf129261770b12d6627909ab
IgorDePaula <principe.borodin@gmail.com>
2015-04-08 09:39:56 -0300

    Removed unnecessary code.

--- Annotations/src/Parser/Parser.php
+++ Annotations/src/Parser/Parser.php
@@ -4,47 +4,27 @@ namespace Annotations\Parser;
 use ArrayObject;
 
 class Parser {
-    private $parserClass;
-    private $args;
+    
     private $parse;
+    
     public function  __construct() {
         $this->parse = new ArrayObject();
     }
 
     public function parse($str){
        preg_match_all("/@(.*) \((.*)\)/\n", $str, $matches);
-       $this->setParserClass($matches[1])->setArguments($matches[2])->fill();
+       $this->fill($matches[1],$matches[2]);
        return $this;
    }
-   
-   protected function setParserClass(array $classes){
-       $this->parserClass = $classes;
-       return $this;
-   }
-   
-   protected function setArguments(array $args){
-       $this->args = $args;
-        return $this;
-   }
-   
-   protected function fill(){
-       $count = count($this->getParserClass());
+      
+   protected function fill(array $classes, array $args){
+       $count = count($classes);
        for($i=0;$i<$count;$i++){     
-           $this->parse->offsetSet(
-                   $this->parserClass[$i],  
-                   new $this->parserClass[$i]($this->args[$i])
-                   );
+           $this->parse->offsetSet($classes[$i], new $classes[$i]($args[$i]));
        }
        return $this;
    }
    
-   function getParserClass() {
-       return $this->parserClass;
-   }
-
-   function getArgs() {
-       return $this->args;
-   }
    function getParse() {
        return $this->parse;
    }
