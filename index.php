411e2f7e9dd95effdb938a4f6192f3e5437333d1
IgorDePaula <principe.borodin@gmail.com>
2018-02-14 14:36:31 -0200

    add class alias

--- index.php
+++ index.php
@@ -5,11 +5,14 @@ use Annotations\Parser\Parser;
 
 $p = new Parser();
 
+class_alias(\Annotations\Parser\Column::class, 'Column');
+class_alias(\Annotations\Parser\Form::class, 'Form');
+$init = microtime(true);
 class Teste
 {
     /**
-     * @Annotations\Parser\Column ({"name":"name","type":"varchar","length":123})
-     * @Annotations\Parser\Form ({"field":"nome"})
+     * @Column ({"name":"name","type":"varchar","length":123})
+     * @Form ({"field":"nome"})
      */
     private $nome;
     /**
@@ -23,8 +26,14 @@ $r = new \ReflectionClass(new Teste());
 $props = $r->getProperties();
 $array = array();
 foreach($props as $prop){
-    var_dump($p->parse($prop->getDocComment())->getParse());
+   // var_dump($p->parse($prop->getDocComment())->getParse());
     $array[$prop->getName()] = $p->parse($prop->getDocComment())->getParse();
 }
+$end = microtime(true);
+echo $end - $init;
+echo PHP_EOL;
 echo '<pre>';
-print_r($array['idade']['Annotations\Parser\Column']->getType());
+print_r($array['idade']['Column']->getType());
+echo PHP_EOL;
+print_r($array['idade']['Form']->getField());
+echo PHP_EOL;
